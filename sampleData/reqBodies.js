3fd76c5ffbe538e9b107b0b9b4137e4c01a09384
nicklascschmidt <nicklas.c.schmidt@gmail.com>
2019-10-29 14:31:38 -0700

    PATCH_UPDATE_REG_GG call added to apiReqs, endpoint added, test req body added but not used

--- sampleData/reqBodies.js
+++ sampleData/reqBodies.js
@@ -1,4 +1,4 @@
-const { POST_CREATE_USER } = require('../constants/apiReqNames');
+const { POST_CREATE_USER, PATCH_UPDATE_REG_GG } = require('../src/constants/apiReqNames');
 
 module.exports[POST_CREATE_USER] = {
   logins: [
@@ -8,4 +8,43 @@ module.exports[POST_CREATE_USER] = {
   ],
   phone_numbers: ['901.111.1111', 'test@synapsefi.com'],
   legal_names: ['Test User'],
+};
+
+/** Currently out of use, need to integrate into test when ready. */
+
+module.exports[PATCH_UPDATE_REG_GG] = {
+  entity_info: {
+    cryptocurrency: true,
+    msb: {
+      federal: true,
+      states: [
+        'AL',
+      ],
+    },
+    public_company: false,
+    majority_owned_by_listed: false,
+    registered_SEC: false,
+    regulated_financial: false,
+    gambling: false,
+    document_id: '{{document_id}}',
+  },
+  signer: {
+    document_id: '{{document_id}}',
+    relationship_to_entity: 'CEO',
+  },
+  compliance_contact: {
+    document_id: '{{document_id}}',
+    relationship_to_entity: 'CEO',
+  },
+  primary_controlling_contact: {
+    document_id: '{{document_id}}',
+    relationship_to_entity: 'CEO',
+  },
+  owners: [
+    {
+      document_id: '{{document_id}}',
+      title: 'CEO',
+      ownership: 95,
+    },
+  ],
 };
