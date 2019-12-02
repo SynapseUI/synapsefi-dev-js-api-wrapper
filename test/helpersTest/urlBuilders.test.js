c72c51d9ff128f2840e4aeea41873d9887d1a9df
Colin Ritchey <colinritchey256@gmail.com>
2019-12-02 15:09:24 -0800

    removed full dehydrate and force refresh as defaulting to on

--- test/helpersTest/urlBuilders.test.js
+++ test/helpersTest/urlBuilders.test.js
@@ -14,6 +14,44 @@ describe('url builders', () => {
     expect(outputUrl).to.equal('origianlUrl/USER_ID/NODE_ID/TRANS_ID');
   });
 
+  it('replacePathParams with both full dehydrate and force refresh', () => {
+    const outputUrl = urlBuilders.replacePathParams({
+      originalUrl: 'origianlUrl/:user_id/:node_id/:trans_id',
+      user_id: 'USER_ID',
+      node_id: 'NODE_ID',
+      trans_id: 'TRANS_ID',
+      full_dehydrate: true,
+      force_refresh: true
+    });
+
+    expect(outputUrl).to.equal('origianlUrl/USER_ID/NODE_ID/TRANS_ID?full_dehydrate=yes&force_refresh=yes');
+  });
+
+  it('replacePathParams with just full dehydrate', () => {
+    const outputUrl = urlBuilders.replacePathParams({
+      originalUrl: 'origianlUrl/:user_id/:node_id/:trans_id',
+      user_id: 'USER_ID',
+      node_id: 'NODE_ID',
+      trans_id: 'TRANS_ID',
+      full_dehydrate: true,
+      force_refresh: false
+    });
+
+    expect(outputUrl).to.equal('origianlUrl/USER_ID/NODE_ID/TRANS_ID?full_dehydrate=yes');
+  });
+  
+  it('replacePathParams with just force refresh', () => {
+    const outputUrl = urlBuilders.replacePathParams({
+      originalUrl: 'origianlUrl/:user_id/:node_id/:trans_id',
+      user_id: 'USER_ID',
+      node_id: 'NODE_ID',
+      trans_id: 'TRANS_ID',
+      force_refresh: true
+    });
+
+    expect(outputUrl).to.equal('origianlUrl/USER_ID/NODE_ID/TRANS_ID?force_refresh=yes');
+  });
+
   it('addQueryParams', () => {
     const outputUrl = urlBuilders.addQueryParams({
       originalUrl: 'origianlUrl',
