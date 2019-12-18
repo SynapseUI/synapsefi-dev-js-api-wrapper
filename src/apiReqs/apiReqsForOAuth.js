8040eef7afc1911e0ab0a44eb5befc89d58778fc
Preston <preston@synapsefi.com>
2019-12-18 15:41:19 -0800

    destruct from object bodyParams

--- src/apiReqs/apiReqsForOAuth.js
+++ src/apiReqs/apiReqsForOAuth.js
@@ -9,8 +9,7 @@ module.exports[POST_OAUTH_USER] = ({ bodyParams, userInfo }) => {
   const { user_id, host, refresh_token, headers } = userInfo;
 
   const reqBody = bodyParams !== undefined ? bodyParams : { refresh_token };
-  const email = bodyParams !== undefined ? bodyParams.email : '';
-  const password = bodyParams !== undefined ? bodyParams.password : '';
+  const { email = '', password = '' } = bodyParams || {};
 
   const isLoginURL = email && password;
 
